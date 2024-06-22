70741e2d86e07667df22f81945c68e54ed85ebad
Caesar <sirostaib@gmail.com>
2024-06-23 01:18:17 +0300

    initial commit
    empty starter uikit project files

---
+++ IKDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IKDB
+//
+//  Created by Siros Taib on 6/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
